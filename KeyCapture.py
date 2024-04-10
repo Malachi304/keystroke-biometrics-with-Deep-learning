b206ee76b75b35a63289a45938277126fb6a6510
Malachi <mpeel9938@gmail.com>
2024-04-10 15:51:17 -0700

    Stable update - Finished KeyCapture?

--- KeyCapture.py
+++ KeyCapture.py
@@ -2,8 +2,8 @@ from pynput.keyboard import Listener, Key       #key capturing, and event listen
 import matplotlib.pyplot as plt    #for plotting data
 import time     #timer for key press
 import pandas as pd
-import csv 
 import numpy as np
+import csv
 
 
 #======================================================================================================================================
@@ -22,12 +22,6 @@ import numpy as np
 
 keys = ['.','t', 'i', 'e', '5', 'o', 'a','n', '1']
 
-#Dataframe for rows
-#df = pd.DataFrame(columns=['subject','sessionIndex','rep','H.period','DD.period.t','UD.period.t','H.t','DD.t.i','UD.t.i','H.i','DD.i.e','UD.i.e','H.e','DD.e.five','UD.e.five','H.five','DD.five.Shift.r','UD.five.Shift.r','H.Shift.r','DD.Shift.r.o','UD.Shift.r.o','H.o','DD.o.a','UD.o.a','H.a','DD.a.n','UD.a.n','H.n','DD.n.l','UD.n.l','H.l','DD.l.Return','UD.l.Return','H.Return'])
-#df.to_csv('myData.csv')
-df = pd.read_csv('myData.csv')
-print(df)
-
 #UPDATE THIS NUMBER EACH SESSION (8 sessions)
 session_count = 1
 #Resets at 50 (50 passwords typed in a session)
@@ -36,8 +30,9 @@ rep_count = 0
 #Each password includes 11 keystrokes. Password + Enter key
 keystroke_count = 0
 
-#List of dictonaries. Each Dictonary is a row/rep (A piece of data from each category) which will be appended to data set
-rep_row = ['1','1','1']
+#new row of data to add to dataset. Already includes userUniqueID, Session count, and Rep count
+rep_row = ['s060',session_count, rep_count]
+
 start_times = {}; 
 hold_time = None
 DownDown = None
@@ -54,30 +49,34 @@ def press(key):
     global rep_count
     global keystroke_count
     global keys
-    global df
+    global rep_row
 
     
     #time of press
     current_time = time.time()
 
     if key == Key.esc:
-        #FIX THIS 
-        # Creating a DataFrame from the list
-        #new_row = pd.DataFrame([rep_row], columns=df.columns)
-        # Appending the new row to the DataFrame
-        df = df._append([rep_row], ignore_index=True)
-
-        # append data frame to CSV file
-        df.to_csv('myData.csv', mode='a', index=False, header=False)
- 
-        # print message
-        print("Data appended successfully.")
-
+     
         return False
 
     #Check if key is type 'char' and in Keys
     if hasattr(key, 'char') and key.char in keys or key == Key.enter or key == Key.shift:
         print(key)
+
+        if key == Key.enter:
+            if rep_count == 3:
+                return
+            rep_count = rep_count + 1
+
+            #append new row to csv       
+            with open('myData.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(rep_row)
+ 
+             # print message
+            print("Data appended successfully.")
+            rep_row = rep_row[:3]
+
         #Essentially if there was a key press prior
         if previous_press_time is not None:
                 #time between last key press and current, ie. time starts when previous key is pressed, and stops when current key is pressed (DD)
@@ -86,7 +85,6 @@ def press(key):
 
                 #adding to row 
                 rep_row.append(DownDown)
-                #keystroke_count = keystroke_count + 1
 
         
             #If there was a key prior
@@ -95,7 +93,6 @@ def press(key):
             time_between = current_time - previous_up_time
             UpDown = time_between
             rep_row.append(UpDown)
-            #keystroke_count = keystroke_count + 1
 
         start_times[key] = current_time
 
@@ -133,9 +130,8 @@ def release(key):
     #Used for UP DOWN time
     previous_up_time = current_time
 
-    # Start the listener
 
-
+# Start the listener
 with Listener(on_press=press, on_release=release) as listener:
     listener.join()
 
