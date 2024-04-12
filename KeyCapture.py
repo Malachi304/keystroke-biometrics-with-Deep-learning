911e15fec0c2c458fd9277cd65f0b2ec6e8e20cf
Malachi <mpeel9938@gmail.com>
2024-04-12 10:30:49 -0700

    clean comments/code, added readMe

--- KeyCapture.py
+++ KeyCapture.py
@@ -1,45 +1,30 @@
 from pynput.keyboard import Listener, Key       #key capturing, and event listener modules from pynput
-import matplotlib.pyplot as plt    #for plotting data
 import time     #timer for key press
-import pandas as pd
-import numpy as np
 import csv
-
-
-#======================================================================================================================================
-#READ ME
-#Password of length 11 (includes enter key at end), typed 50 times across 8 sessions, totalling 400 unique key strokes.
-#Day between each session totalling 8 days if done concurently.
-#Program uses key press/release times to record total key HOLD (H) time, UP DOWN (UD) time, and DOWN DOWN (DD) time (Explained further in code comments).
-#Data is then appended to Data.csv for training. 
-
-# password for referance :  .tie5Roanl
-
-#Categories from Data set for referance : 
-#subject,sessionIndex,rep,H.period,DD.period.t,UD.period.t,H.t,DD.t.i,UD.t.i,H.i,DD.i.e,UD.i.e,H.e,DD.e.five,UD.e.five,H.five,DD.five.Shift.r,
-#UD.five.Shift.r,H.Shift.r,DD.Shift.r.o,UD.Shift.r.o,H.o,DD.o.a,UD.o.a,H.a,DD.a.n,UD.a.n,H.n,DD.n.l,UD.n.l,H.l,DD.l.Return,UD.l.Return,H.Return
-#======================================================================================================================================
 
 keys = ['.','t', 'i', 'e', '5', 'o', 'a','n', '1']
 
 #UPDATE THIS NUMBER EACH SESSION (8 sessions)
-session_count = 1
+session_count = 3
 #Resets at 50 (50 passwords typed in a session)
 rep_count = 0
 
 #new row of data to add to dataset. Already includes userUniqueID, Session count, and Rep count
 rep_row = ['s060',session_count, rep_count]
 
+#holds times of current keystroke 
 start_times = {}; 
+#features
 hold_time = None
 DownDown = None
 UpDown = None
 
+#used to calculate features from UD and DD
 previous_press_time = None
 previous_up_time = None
 
+#on press
 def press(key):
-    #Access varible globally
     global previous_press_time
     global DownDown
     global UpDown
@@ -52,14 +37,13 @@ def press(key):
     #time of press
     current_time = time.time()
 
+    #end session early
     if key == Key.esc:
-     
         return False
 
     #Check if key is type 'char' and in Keys
     if hasattr(key, 'char') and key.char in keys or key == Key.enter or key == Key.shift:
         print(key)
-
 
         #Essentially if there was a key press prior
         if previous_press_time is not None:
@@ -67,25 +51,25 @@ def press(key):
                 time_between = current_time - previous_press_time
                 DownDown = time_between
 
-                #adding to row 
+                #adding to row
                 rep_row.append(DownDown)
 
         
-            #If there was a key prior
+        #If there was a key prior
         if previous_up_time is not None:
             #This serves as Up Down time, ie. Timer starts when previous key is let go, and stops when current key is pressed (UD)
             time_between = current_time - previous_up_time
             UpDown = time_between
+            #adding to row
             rep_row.append(UpDown)
 
+        #Moment key is pressed
         start_times[key] = current_time
 
         #Update previous time as current time (Current key will become previous key for the next key press)
         previous_press_time = current_time
-     
-
-        
-        #if key is not 'shift,' or in Keys
+    
+    #If key is not 'shift,' or in Keys
     elif key not in keys:
         print("Invalid")
     
@@ -102,41 +86,51 @@ def release(key):
     global previous_press_time
     global DownDown
     global UpDown
+
     #time of release
     current_time = time.time()
 
    
-    #If released key is pressed key (insured with listner.join)
+    #If released key is pressed key
     if hasattr(key, 'char') and key.char in keys or key == Key.enter or key == Key.shift:
+
         #Hold time. ie. time starts when key is pressed, and stops when key is lifted (H)
         time_between = current_time - start_times[key]
         hold_time = time_between
         rep_row.append(hold_time)
 
-        #Used for UP DOWN time
+        #Used for UP DOWN time, when previous key was released 
         previous_up_time = current_time
 
+        #If password is entered and correct number of row attributes are present
         if key == Key.enter and len(rep_row) == 34:
+            #reset all time stamps
             previous_press_time = None
             previous_up_time = None
             hold_time = None
             DownDown = None
             UpDown = None
 
+            #End session
             if rep_count == 50:
                 print("Session Complete")
                 return
             rep_count = rep_count + 1
 
             #append new row to csv       
-            with open('myData.csv', 'a', newline='') as file:
+            with open('newData.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(rep_row)
  
-             # print message
+            #print appened message
             print(f"Data appended successfully. REP: {rep_count}, Data length: {len(rep_row)}")
+            #reset for next row of data
             rep_row.clear()
             rep_row = ['s060',session_count, rep_count]
+
+        #This is essentially a fail-safe. 
+        #If we press the enter key before the row is completed, it resets that row
+        #Utalize this for when incorrect keys are pressed, such as 't' key being pressed too early.
         elif key == Key.enter and len(rep_row) != 34:
             previous_press_time = None
             previous_up_time = None
@@ -147,38 +141,11 @@ def release(key):
             rep_row = ['s060',session_count, rep_count]
             print("Invalid, reseting")
 
-
-
         #clear key in preporation for next
         del start_times[key]
     
 
-
-
-
-
-    # Start the listener
+# Start the listener
 with Listener(on_press=press, on_release=release) as listener:
     listener.join()
 
-
-'''
-        if key == Key.enter and len(rep_row)==34:
-            if rep_count == 50:
-                return
-            rep_count = rep_count + 1
-
-            #append new row to csv       
-            with open('myData.csv', 'a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(rep_row)
- 
-             # print message
-            print(f"Data appended successfully. REP: {rep_count}")
-            rep_row = ['s060',session_count, rep_count]
-        elif key == Key.enter and len(rep_row) != 34:
-
-            rep_row = ['s060',session_count, rep_count]
-            print(len(rep_row))
-            print("Invalid, reseting")
-'''
---
+++ ReadMe.md
@@ -0,0 +1,39 @@
+
+# Analysis Of Keystoke Authentication Across Different Systems Utalizing Deep Learning 
+
+## GOAL:
+==================================================================================================================================================
+Deep learning model for keystroke authenticaion across different systems (keyboards).
+
+## METHOD:
+==================================================================================================================================================
+Dataset is trained on over 50 typists. Specifics for how that data was collected by origional researchers is below. 
+
+I collected my own keystroke data using a self-made keystoke capturing python script. My keystrokes are added to the training dataset in order to predict 
+my keystroke patterns when typing a specific password mentioned below.
+
+After training, I will pass a new set of keystrokes for prediction, using two seperate keyboards.This will measure the potential accuracy of deep learning models
+with keystokes across different systems. 
+
+## DETAILS OF DATA CAPTURE AND PROCESSING:
+==================================================================================================================================================
+
+password for referance :  .tie5Roanl
+
+Password of length 11 (includes 'enter' key at end), typed 50 times across 8 sessions, totaling 400 unique key strokes.
+Day between each session totalling 8 days if done concurently.
+
+KeyCapture program uses key press/release times to record total key HOLD (H) time, UP DOWN (UD) time, and DOWN DOWN (DD) time (Explained further in code).
+Data is then appended to newData.csv, before being processed in ProcessData.py, and then being output as ProcessedData.csv. 
+ProcessedData is then combined with the orional data of over 50 unique typists before training. 
+
+Categories from Data set for referance : 
+subject,sessionIndex,rep,H.period,DD.period.t,UD.period.t,H.t,DD.t.i,UD.t.i,H.i,DD.i.e,UD.i.e,H.e,DD.e.five,UD.e.five,H.five,DD.five.Shift.r,
+UD.five.Shift.r,H.Shift.r,DD.Shift.r.o,UD.Shift.r.o,H.o,DD.o.a,UD.o.a,H.a,DD.a.n,UD.a.n,H.n,DD.n.l,UD.n.l,H.l,DD.l.Return,UD.l.Return,H.Return
+
+NOTE: Subject is the label we are training for, rep and sessionindex are not important for training and may be redacted.
+      The final 31 Categories are the features being passed to the model. 
+
+      Example of features: "UD.period.t" traslates to time between releasing '.' key (UPtime), and time pressing 't' key (DOWNtime)
+
+==================================================================================================================================================
