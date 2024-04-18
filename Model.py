cd06c12e92d859263f8cca956f4388d87f557453
Malachi <mpeel9938@gmail.com>
2024-04-17 22:00:02 -0700

    working binary classification model

--- Model.py
+++ Model.py
@@ -1,6 +1,4 @@
-import tensorflow as tf
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
@@ -9,17 +7,28 @@ from sklearn.preprocessing import LabelEncoder
 
 
 
+
 data = pd.read_csv('data/Data.csv')
+data.drop(columns=['sessionIndex', 'rep'], inplace=True)
+
+
+# Select a specific subject as your target variable
+target_subject = 's002'
+
+# Encode the target variable to represent the presence or absence of the specific subject
+data['target'] = (data['subject'] == target_subject).astype(int)
+# print(data.tail())
 
 # Initialize LabelEncoder
-label_encoder = LabelEncoder()
+# label_encoder = LabelEncoder()
 
 # Encode the 'subject' column
-data['subject'] = label_encoder.fit_transform(data['subject'])
+# data['subject'] = label_encoder.fit_transform(data['subject'])
+# data['target'] = (data['subject'] == target_subject).astype(int)
 
 
-x = data.drop(columns=['subject'])
-y = data['subject']
+x = data.drop(columns=['subject', 'target'])
+y = data['target']
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -30,15 +39,13 @@ X_test_scaled = scaler.transform(X_test)
 
 # Define the model architecture
 model = Sequential([
-    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    Dense(64, activation='relu'),
-    Dense(len(y.unique()), activation='softmax')  # Output layer with softmax activation for multi-class classification
+    Dense(32, activation='relu', input_shape=(31,)),
+    Dense(32, activation='relu'),
+    Dense(1, activation='sigmoid')  
 ])
 
 # Compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
 # Train the model
 model.fit(X_train_scaled, y_train, epochs=10, batch_size=32, validation_split=0.1)
--- ProcessData.py
+++ ProcessData.py
@@ -9,11 +9,18 @@ df = pd.read_csv('data/newData.csv')
 #round decimal place
 df = df.round(4)
 
- 
+df1 = pd.read_csv('data/Data.csv')
+
+# Concatinate origional data, and new data
+combined_df = pd.concat([df1, df], axis=0)
+combined_df.drop(columns=['sessionIndex', 'rep'], inplace=True)
+
+
+
 # Remove rows where any element equals zero
 # Happens on occasion (Unkown issue, possibly latency), data does not need to be replaced until after all sessions are complete
 # REMOVED FEATURE because some of the origional data has negative values
-#df_processed = df[(df != 0).all(axis=1)]
+# df_processed = df[(df != 0).all(axis=1)]
 
 # Save the filtered dataset
 #df_processed.to_csv('ProcessedData.csv', index=False)
