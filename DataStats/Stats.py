3f01f1763ecad999b55c169f43cf7da5c2d28fe9
Malachi <mpeel9938@gmail.com>
2024-04-19 12:03:22 -0700

    outlier information

--- DataStats/Stats.py
+++ DataStats/Stats.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt 
+import numpy as np
 
 df_new = pd.read_csv('Data/newData.csv')
-df_origional = pd.read_csv('Data/Data.csv')
+df_original = pd.read_csv('Data/Data.csv')
 
 # Combined, and Preprocessed data
 df_processed = pd.read_csv('Data/ProcessedData.csv')
@@ -13,28 +13,45 @@ df_processed = pd.read_csv('Data/ProcessedData.csv')
 df_processed.drop(columns=['subject', 'target'], inplace=True)
 # Heatmap of processed data 
 sns.heatmap(df_processed.corr())
-plt.show()
+#plt.show()
 
 
 
 df_new.drop(columns=['subject','sessionIndex', 'rep'], inplace=True)
-df_origional.drop(columns=['subject', 'sessionIndex', 'rep'], inplace=True)
+df_original.drop(columns=[ 'subject', 'sessionIndex', 'rep'], inplace=True)
+#print(df_new.info())
+#print(df_original.info())
 
-correlation_matrix = df_new.corrwith(df_origional)
+
+correlation_matrix = df_new.corrwith(df_original)
 
 # Print correlation matrix
-print(correlation_matrix)
+#print(correlation_matrix)
 
 # Assuming df_original is your DataFrame
 # Calculate z-scores for each column
-# z_scores = (df_origional - df_origional.mean()) / df_origional.std()
+z_scores_original = (df_original - df_original.mean()) / df_original.std()
+z_scores_new = (df_new - df_new.mean()) / df_new.std()
 
-# Define threshold for outlier detection (e.g., z-score greater than 3 or less than -3)
-#threshold = 3
+# Define threshold for outlier detection (z-score greater than 3 or less than -3)
+threshold = 3
 
 # Identify outliers
-#outliers = np.abs(z_scores) > threshold
+outliers_original = np.abs(z_scores_original) > threshold
+outliers_new = np.abs(z_scores_new) > threshold
+
 
 # Print rows corresponding to outliers
-#print("Outliers:")
-#print(df_original[outliers.any(axis=1)])
+print("Outliers Original:")
+print(len(df_original[outliers_original.any(axis=1)]))
+# Print rows corresponding to outliers
+print("Outliers New:")
+print(len(df_new[outliers_new.any(axis=1)]))
+
+tdf0 = df_original[outliers_original.any(axis=1)]
+tdf1 = df_new[outliers_new.any(axis=1)]
+
+tdf0.to_csv("Ouliers_og.csv")
+tdf1.to_csv("Ouiers_new.csv")
+
+
--- ProcessData.py
+++ ProcessData.py
@@ -3,44 +3,23 @@
 #==============================================================================
 
 import pandas as pd
+from DataStats.Stats import Outliers_new, Outliers_original
 
 # Load the new dataset captured from KeyCapture sessions
 df = pd.read_csv('data/newData.csv')
 df1 = pd.read_csv('data/Data.csv')
-
+print(len(Outliers_new))
+print(len(Outliers_original))
 # Round decimal place
-df= df.round(4)
+#df= df.round(4)
 # Combined_df = df.applymap(lambda x: "{:.4f}".format(x))
 
 # Concatinate origional data, and new data
-combined_df = pd.concat([df1, df], axis=0)
-combined_df.drop(columns=['sessionIndex', 'rep'], inplace=True)
+#combined_df = pd.concat([df1, df], axis=0)
+#combined_df.drop(columns=['sessionIndex', 'rep'], inplace=True)
 
 # Label encoding subject, 1 for newData, 0 for origional data
-target_subject = 's060'
-combined_df['target'] = (combined_df['subject'] == target_subject).astype(int)
-combined_df.to_csv('Data/ProcessedData.csv', index=False)
-
-
-#==============================================================================
-# Stats on data, such as correlation between data sets
-#==============================================================================
-
-import seaborn as sns
-import matplotlib.pyplot as plt 
-
-heat_map_data = combined_df
-heat_map_data = heat_map_data.drop(['subject'], axis=1) 
-# Heatmap of combined data set
-sns.heatmap(heat_map_data.corr())
-plt.show()
-
-
-df_test = df.drop(['subject'], axis=1)
-df1_test = df1.drop(['subject'], axis=1)
-
-correlation_matrix = df_test.corrwith(df1_test)
-
-# Print correlation matrix
-print(correlation_matrix)
+#target_subject = 's060'
+#combined_df['target'] = (combined_df['subject'] == target_subject).astype(int)
+#combined_df.to_csv('Data/ProcessedData.csv', index=False)
 
